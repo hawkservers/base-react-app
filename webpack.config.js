095eabc0039339a12c1fb69cc8496d43b5b101e1
stubbo <georgestubbs24@gmail.com>
2021-10-30 21:51:46 +0100

    Improved tsconfig and webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,47 +4,53 @@ const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ReactRefreshTypeScript = require('react-refresh-typescript');
 
-const isDevelopment = process.env.NODE_ENV !== 'production';
+module.exports = (env, argv) => {
+  const isDevelopment = argv.mode !== 'production';
 
-module.exports = {
-  mode: isDevelopment ? 'development' : 'production',
-  entry: {
-    main: './src/index.tsx',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        include: path.join(__dirname, 'src'),
-        use: [
-          {
-            loader: 'ts-loader',
-            options: {
-              configFile: 'tsconfig.json',
-              transpileOnly: true,
-              ...(isDevelopment && {
-                getCustomTransformers: () => ({
-                  before: [ReactRefreshTypeScript()],
+  return {
+    mode: isDevelopment ? 'development' : 'production',
+    entry: {
+      main: ['./src/index.tsx', './src/Styles/style.css'],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          include: path.join(__dirname, 'src'),
+          use: [
+            {
+              loader: 'ts-loader',
+              options: {
+                configFile: 'tsconfig.json',
+                transpileOnly: true,
+                ...(isDevelopment && {
+                  getCustomTransformers: () => ({
+                    before: [ReactRefreshTypeScript()],
+                  }),
                 }),
-              }),
+              },
             },
-          },
-        ],
-      },
-    ],
-  },
-  plugins: [
-    isDevelopment && new ReactRefreshPlugin(),
-    new ForkTsCheckerWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      filename: './index.html',
-      template: './public/index.html',
-    }),
-  ],
-  resolve: {
-    extensions: ['.js', '.ts', '.tsx'],
-  },
-  devServer: {
-    historyApiFallback: true,
-  },
+          ],
+        },
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader', 'postcss-loader'],
+        },
+      ],
+    },
+    plugins: [
+      isDevelopment && new ReactRefreshPlugin(),
+      new ForkTsCheckerWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        filename: './index.html',
+        template: './public/index.html',
+      }),
+    ].filter(Boolean),
+    resolve: {
+      extensions: ['.js', '.ts', '.tsx'],
+    },
+    devServer: {
+      historyApiFallback: true,
+    },
+  };
 };
